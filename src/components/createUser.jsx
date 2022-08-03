2fc1e93b9e936ddb0072cc0f38377efba0c5585c
saeeda-kh <saeeda.khatoon230@gmail.com>
2022-08-03 12:36:38 +0500

    applied react hook form on createUser form

--- src/components/createUser.jsx
+++ src/components/createUser.jsx
@@ -1,60 +1,74 @@
 import React from "react";
-import axios from "axios";
 import "./excercises.css";
-import { useRef } from "react";
+import { useForm } from "react-hook-form";
 
 export const CreateUser = () => {
-  const name = useRef("");
-  const email = useRef("");
-  const age = useRef("");
-  const passward = useRef("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({defaultValues: {
+    name : "",
+    email : "",
+    age: "",
+    passward: ""
+  }});
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name.current.value);
-    axios
-      .post("http://localhost:3001/users/add", {
-        username: name.current.value,
-        email: email.current.value,
-        age: age.current.value,
-        passward: passward.current.value,
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-    name.current.value = "";
-    email.current.value = "";
-    age.current.value = "";
-    passward.current.value = "";
-  };
+  console.log(watch("age"));
 
   return (
     <div className="form">
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
         <div className="mb-3">
           <label>UserName</label>
-          <input type={"text"} ref={name} className="form-control" required />
+          <input
+            type={"text"}
+            {...register("name", { required: "This field is required" })}
+            className="form-control"
+            placeholder="required"
+          />
         </div>
         <div className="mb-3">
           <label>Email</label>
-          <input type={"text"} ref={email} className="form-control" required />
+          <input
+            type={"text"}
+            {...register("email", { required: "This field is required" })}
+            className="form-control"
+            placeholder="required"
+          />
+          {errors.name?.message}
+          
         </div>
         <div className="mb-3">
           <label>Age</label>
-          <input type={"text"} ref={age} className="form-control" required />
+          <input
+            type={"number"}
+            {...register("age", {required:"required field" , min:18 , max: 90, 
+            maxLength:{value: "3", message: "max length exceed"}})}
+            className="form-control"
+            
+          />
+         <p>{errors.age?.message}</p> 
+       
         </div>
         <div className="mb-3">
           <label>Passward</label>
           <input
             type="password"
-            ref={passward}
+            {...register("passward", {
+              required: "This field is required",
+              minLength: { value: 5, message: "enter at least 5 characters" },
+            })}
             className="form-control"
-            required
+            placeholder="required"
           />
+           <p>{errors.passward?.message}</p> 
+
         </div>
 
         <button type="submit" className="btn btn-primary">
